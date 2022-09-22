764e0cb1b61ea98f4da080a11e78baaece93d58a
DESKTOP-T5EG0UE\user <chohmk>
2022-09-22 21:44:10 +0900

    no message

--- src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
+++ src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
@@ -2,6 +2,7 @@ package com.example.lesson03;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.lesson03.bo.ReviewBO;
@@ -12,9 +13,15 @@ public class Lesson03Ex01RestController {
 
 	@Autowired
 	private ReviewBO reviewBO;
+
 	// 요청URL: http://localhost/lesson03/ex01
 	@RequestMapping("/lesson03/ex01")
-	public Review ex01() {
-		return reviewBO.getReviewById(5);
+	public Review ex01(
+			@RequestParam("id") int id // 필수 파라미터
+			// @RequestParam(value="id") int id // 필수 파라미터
+			// @RequestParam(value ="id", required = false) Integer id // 비필수 파라미터
+			// @RequestParam(value="id", defaultValue="1") int id		// 비필수 파라미터, 디폴트값 1
+	) {
+		return reviewBO.getReviewById(id);
 	}
 }
