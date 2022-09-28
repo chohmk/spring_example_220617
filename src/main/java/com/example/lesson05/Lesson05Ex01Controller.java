11929459d24ac868ccd8da94dfe96d7621f48cf8
DESKTOP-T5EG0UE\user <chohmk>
2022-09-28 21:43:03 +0900

    예제

---
+++ src/main/java/com/example/lesson05/Lesson05Ex01Controller.java
@@ -0,0 +1,60 @@
+package com.example.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson05")
+@Controller
+public class Lesson05Ex01Controller {
+
+	@RequestMapping("/ex01")
+	public String ex01() {
+		return "lesson05/ex01";
+	}
+	
+	@RequestMapping("/ex02")
+	public String ex02(Model model) {
+		// list<String>
+		List<String> fruits = new ArrayList<>();
+		fruits.add("apple");
+		fruits.add("melon");
+		fruits.add("peach");
+		fruits.add("grape");
+		fruits.add("mango");
+		
+		model.addAttribute("fruits", fruits);
+		
+		// List<Map>
+		List<Map<String, Object>> users = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "조현");
+		map.put("age", "28");
+		map.put("hobby", "축구");
+		users.add(map);
+		
+		map = new HashMap<>();
+		map.put("name", "바다");
+		map.put("age", "23");
+		map.put("hobby", "게임");
+		users.add(map);
+		
+		model.addAttribute("users", users);
+		
+		
+		
+		
+		
+		
+		
+		return "lesson05/ex02";
+	}
+	
+	
+
+}
